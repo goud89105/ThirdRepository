8aa3db5e45896a750dd6fc7210376bd59485756e
ravindra <ravindra.goud1989@gmail.com>
2023-11-07 11:52:56 +0530

    ZFKW-667-added some method

--- src/main/java/com/thirdexample/Third.java
+++ src/main/java/com/thirdexample/Third.java
@@ -2,6 +2,10 @@ package com.thirdexample;
 
 public class Third {
 
+	public void demo()
+	{
+		System.out.println("ddddfffff");
+	}
 	public static void main(String[] args) {
 		System.out.println("demooooooo");
 	}
